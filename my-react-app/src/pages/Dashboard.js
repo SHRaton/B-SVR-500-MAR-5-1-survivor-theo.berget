99573f302c910dda961f73fc120e2d476e500f6d
Alexandre <alexandre.vittenet@epitech.eu>
2024-09-06 00:53:34 +0200

    Dashboard basic

--- my-react-app/src/pages/Dashboard.js
+++ my-react-app/src/pages/Dashboard.js
@@ -6,12 +6,24 @@ function Dashboard() {
     <div>
       <h1>Dashboard</h1>
       <p>Welcome!</p>
-      <div className="mainContainer">
-        <div className="container">
-          <div className="bloc1"></div>
-          <div className="bloc2"></div>
-          <div className="bloc3"></div>
-          <div className="bloc4"></div>
+      <div className="mainContainerDash">
+        <div className="bloc1">
+          <div className="d1">
+            <p className="titleDash">Customers Overview</p>
+            <p className="subtitleDash">When a customers have joined in the time.</p>
+          </div>
+          <div className="d2">
+            <p className="titleDash">Events</p>
+            <p className="subtitleDash">Our events and their status.</p>
+          </div>
+        </div>
+        <div className="bloc2">
+          <div className="d3">
+            <p className="titleDash">Customers by Country</p>
+          </div>
+          <div className="d4">
+            <p className="titleDash">Meetings top sources</p>
+          </div>
         </div>
       </div>
     </div>
