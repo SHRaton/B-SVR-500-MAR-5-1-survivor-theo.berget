f2c300ad4d0ba430f6c7c1dd252894b12f4bdc54
MeddiGU <meddi.gueran@epitech.eu>
2024-09-05 11:36:59 +0200

    New route

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,6 +9,8 @@ import Events from "./pages/Events";
 import Login from "./pages/Login";
 import ClientDetails from "./pages/ClientDetails";
 import AddCustomers from "./pages/addCustomers";
+import Astro from "./pages/Astro"
+import ResultPage from './pages/ResultPage'
 import './App.css';
 
 function App() {
@@ -26,7 +28,9 @@ function App() {
             <Route path="/AddCustomers" element={<AddCustomers /> }/>
             <Route path="/tips" element={<Tips />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/astro" element={<Astro />} />
             <Route path="/customers/:id" element={<ClientDetails />} />
+            <Route path="/result" element={<ResultPage />} />
           </Routes>
         </div>
       </div>
